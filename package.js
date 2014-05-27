541ba6370b7ea7c7492a24530116fc7edd1eb860
rainbough <rainbough@gmail.com>
2014-05-27 16:07:34 -0500

    update member file directory

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.on_use(function (api) {
 
   	api.use([ 'iron-router', 'bootstrap-3', 'errors'], ['client']);
 
-    api.add_files(['new_member.html', 'members/members_list.html', 'members/member_link.html', 'members/member_page.html','members/member_profile.html','members/member_edit.html','new_member.js', 'member_router.js'], 'client');
+    api.add_files(['members/new_member.html', 'members/members_list.html', 'members/member_link.html', 'members/member_page.html','members/member_profile.html','members/member_edit.html','members/new_member.js', 'members/member_router.js'], 'client');
     api.add_files('members.js', ['client', 'server']);
     api.export('Members', ['client', 'server']);
 });
